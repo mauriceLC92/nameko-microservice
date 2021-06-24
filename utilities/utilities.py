c3313b880f1cbcbcae566eb2febf426265b4c5bf
Maurice Le Cordier <mauricelecordier@gmail.com>
2021-06-24 13:04:19 +0200

    added a working version of the string compression using a huffman lib

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,5 +1,7 @@
 from typing import List
 from nameko.rpc import rpc
+from dahuffman import load_shakespeare
+import ast
 
 
 class UtilitiesService:
@@ -8,3 +10,15 @@ class UtilitiesService:
     @rpc
     def square_each_odd_number(self, nums_arr: List[int]) -> List[int]:
         return [num * num for num in nums_arr if num % 2 != 0]
+
+    @rpc
+    def encode(self, words: List[str]) -> dict:
+        codec = load_shakespeare()
+        encoded_words = [{item: str(codec.encode(item))} for item in words]
+        return encoded_words
+
+    @rpc
+    def decode(self, encoded_word: str) -> str:
+        codec = load_shakespeare()
+        decoded = codec.decode(ast.literal_eval(encoded_word))
+        return decoded
